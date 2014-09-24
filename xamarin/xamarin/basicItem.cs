15b5f8ec3307fced2475034053a17e66f5674700
Sten Hougaard <netsi1964@gmail.com>
2014-09-24 22:57:02 +0200

    First commit
    This is the first commit pushed from Xamarin and committed from Github
    OS X app to Github.

---
+++ xamarin/xamarin/basicItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dynamicweb.Content.Items;
+using Dynamicweb;
+
+namespace xamarin
+{
+	[Dynamicweb.Content.Items.Annotations.Name ("Basic Item")]
+	public class basicItem
+	{
+		public string Heading { get; set; }
+
+		public int Integer { get; set; }
+	}
+}
