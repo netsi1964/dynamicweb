c1de3f8c5af46f3d9ba2da99863e022fb831240e
Sten Hougaard <netsi1964@gmail.com>
2014-09-25 00:12:36 +0200

    Minor updates

--- xamarin/xamarin/basicItem.cs
+++ xamarin/xamarin/basicItem.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using Dynamicweb.Content.Items;
 using Dynamicweb;
+
+// Used for Item related attributes
 using Dynamicweb.Content.Items.Annotations;
 
- // Used for Item related attributes
+// Used if you want custom editors
+using Dynamicweb.Content.Items.Editors;
+
+// Used for item metadata
+using Dynamicweb.Content.Items.Metadata;
+
+
 
 namespace xamarin
 {
 	[Name ("Basic Item")]
-	public class basicItem : ItemEntry
+	public class basicItem : ItemEntry  // classname becomes "systemname" used among other in Database Table Name "ItemType_basicItem"
 	{
+
+
 		[Group ("Content")] // This field will be placed in a group named "Content"
 		public string Heading { get; set; }
 
 		[Group ("Content")]
 		public int Integer { get; set; }
 
+		[Group ("Content")]
 		[Field ("Content", typeof(Dynamicweb.Content.Items.Editors.DateTimeEditor))]
+		[Name ("Born")]
 		public DateTime Born { 
 			get { return this.Born; }
 			set { 
@@ -25,6 +37,10 @@ namespace xamarin
 			}
 		}
 
+		[Group ("Content")]
+		[Field ("HTML Body", typeof(SimpleTextboxEditor))]
+		public string Body { get; set; }
+
 		// Here the set will only allow for 155 chars
 		[Group ("SEO")]
 		public string MetaDescription { 
@@ -32,7 +48,41 @@ namespace xamarin
 			set { this.Heading = value.Substring (0, 155); } 
 		}
 
+		// http://developer.dynamicweb.com/documentation/for-developers/item-based-structure/working-with-metadata.aspx
+		// ItemType t = ItemManager.Metadata.GetItemType ("basicItem");
 
+
+		// Returns the current metadata
+		public static MetadataContainer GetMeta ()
+		{
+			return ItemManager.Metadata.GetMetadata ();
+		}
+
+	}
+
+	// http://developer.dynamicweb.com/documentation/for-developers/item-based-structure/using-field-editors.aspx
+	[Editor ("My custom editor")]
+	public class SimpleTextboxEditor : Editor
+	{
+		public override Type DataType {
+			get { return typeof(string); }
+		}
+
+		public override void BeginEdit (EditorContext context)
+		{
+			if (context != null && context.Output != null) {
+				var v = context.Value != null ?
+					context.Value.ToString () : string.Empty;
+
+				context.Output.WriteLine (string.Format ("<input type=\"text\" value=\"{0}\" />",
+					System.Web.HttpUtility.HtmlAttributeEncode (v)));
+			}
+		}
+
+		public override object EndEdit ()
+		{
+			return base.HttpContext.Request [base.Key];
+		}
 	}
 
 
