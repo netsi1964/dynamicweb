68ac5520b4ffce5919f91c60cc23da3e23c795eb
Sten Hougaard <netsi1964@gmail.com>
2014-09-24 23:20:28 +0200

    Basic Item updated
    Added Groups and a Datetime which checks if entered born date is not
    older than 120 years.

--- xamarin/xamarin/basicItem.cs
+++ xamarin/xamarin/basicItem.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using Dynamicweb.Content.Items;
 using Dynamicweb;
+using Dynamicweb.Content.Items.Annotations;
+
+ // Used for Item related attributes
 
 namespace xamarin
 {
-	[Dynamicweb.Content.Items.Annotations.Name ("Basic Item")]
+	[Name ("Basic Item")]
 	public class basicItem : ItemEntry
 	{
+		[Group ("Content")] // This field will be placed in a group named "Content"
 		public string Heading { get; set; }
 
+		[Group ("Content")]
+		public int Integer { get; set; }
+
+		[Field ("Content", typeof(Dynamicweb.Content.Items.Editors.DateTimeEditor))]
+		public DateTime Born { 
+			get { return this.Born; }
+			set { 
+				DateTime oldest = DateTime.Now.AddYears (-120);
+				this.Born = (this.Born < oldest) ? this.Born : oldest;
+			}
+		}
+
 		// Here the set will only allow for 155 chars
+		[Group ("SEO")]
 		public string MetaDescription { 
 			get { return this.MetaDescription; } 
 			set { this.Heading = value.Substring (0, 155); } 
 		}
 
-		public int Integer { get; set; }
+
 	}
 
 
