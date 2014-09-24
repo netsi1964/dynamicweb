6e6698a4d9cf7e3aa6c6f1f600688ba2454662ac
Sten Hougaard <netsi1964@gmail.com>
2014-09-24 23:05:55 +0200

    Added field MetaDescription
    The first .net item is updated with a field MetaDescription
    which on then setter will enforce a maximum of 155 chars.

--- xamarin/xamarin/basicItem.cs
+++ xamarin/xamarin/basicItem.cs
@@ -5,10 +5,18 @@ using Dynamicweb;
 namespace xamarin
 {
 	[Dynamicweb.Content.Items.Annotations.Name ("Basic Item")]
-	public class basicItem
+	public class basicItem : ItemEntry
 	{
 		public string Heading { get; set; }
 
+		// Here the set will only allow for 155 chars
+		public string MetaDescription { 
+			get { return this.MetaDescription; } 
+			set { this.Heading = value.Substring (0, 155); } 
+		}
+
 		public int Integer { get; set; }
 	}
+
+
 }
